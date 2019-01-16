8bb9092cf1703d4840f9ce966e441dde28be0500
McKimbo <44180982+McKimbo@users.noreply.github.com>
2019-01-16 10:03:55 -0500

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,15 @@
+public class Hello{
+	public static void printHeading(){
+		System.out.println("Christian Kim");
+		System.out.println("Hello");
+		System.out.println("Program that prints \"Hello\"");
+		System.out.println("Jan 16, 2018");
+		System.out.println("CMSC 255 - 003");
+		System.out.println("");
+	}
+	public static void main(String [] args){
+		printHeading();
+		System.out.println("Hello");
+
+	}
+}
